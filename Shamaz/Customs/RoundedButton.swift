f3c2d22e72d7eb580ee38bee7477cbb1ba2e2ede
Gabriel Balta <g.balta96@gmail.com>
2020-02-21 01:27:19 +0000

    Initial commit. LaunchScreen complete, Custom RoundedButton swift file add

---
+++ Shamaz/Customs/RoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedButton.swift
+//  Shamaz
+//
+//  Created by Gabriel Balta on 21/02/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
