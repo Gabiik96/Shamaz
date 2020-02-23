2b4562bfa505eeb0d7aa81e04f95dcb1be41d513
Gabriel Balta <g.balta96@gmail.com>
2020-02-23 17:57:26 +0000

    Logic file created, arrays added

---
+++ Shamaz/Customs/Logic.swift
@@ -0,0 +1,26 @@
+//
+//  Logic.swift
+//  Shamaz
+//
+//  Created by Gabriel Balta on 23/02/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import Foundation
+
+let pastStartArray = ["How was your best moment in last",
+                      "What extraordinary happend to you in last",
+                      "Describe your worst experience in last",
+                      "Where have you been at in last"]
+
+let futureStartArray = ["What is your goal in next",
+                        "Which person would you like to see in next",
+                        "Where will you be in next",
+                        "How many movies will you watch in next"]
+
+let endArray = ["day", "days",
+                "week", "weeks",
+                "month", "months",
+                "year", "years"]
+
+let randomNumber = Int.random(in: 1...10)
